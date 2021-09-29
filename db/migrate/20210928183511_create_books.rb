4362e8bfbb16269ebb0c72e34c191a679ee19748
Mark Link II <55418610+marklinkii@users.noreply.github.com>
2021-09-29 14:29:10 -0500

    uncomment

--- db/migrate/20210928183511_create_books.rb
+++ db/migrate/20210928183511_create_books.rb
@@ -1,13 +1,13 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
-  #def change
-  #  create_table :books do |t|
-  #    t.string :title
-  #    t.string :author
-  #    t.decimal :price
-  #    t.datetime :published_date
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.decimal :price
+      t.datetime :published_date
 
-  #    t.timestamps
-  #  end
-  #end
+      t.timestamps
+    end
+  end
 end
 # db schema
