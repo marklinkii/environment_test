620e4c02d521d206060d1ffce9df62d0a35bc727
Mark Link II <markuslink@tamu.edu>
2021-09-30 10:48:21 -0500

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :books
   root 'books#index'
-  member do
-    get :delete
-  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
